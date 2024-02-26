cbfb1ad501a4c7feaf120a449bcd556118104a10
Zoe Silver <zsilver@usc.edu>
2024-02-26 11:40:14 -0800

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -72,14 +72,3 @@ def a_star_search(stack):
 
     # ---------------------------- #
 
-
-def weighted_a_star_search(stack, epsilon=None, N=1):
-    # Weighted A* is extra credit
-
-    flip_sequence = []
-
-    # --- v ADD YOUR CODE HERE v --- #
-
-    return flip_sequence
-
-    # ---------------------------- #
